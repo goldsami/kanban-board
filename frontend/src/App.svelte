6629d122c9af541dc088c3298454bc380650bd0b
goldsami <zolotarenkomax@gmail.com>
2022-01-29 20:26:00 +0200

    feat: users query added

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,38 @@
 <script lang="ts">
+	import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
+	import { setClient, query } from "svelte-apollo";
+
 	export let name: string;
+
+	const client = new ApolloClient({
+		uri: "http://localhost:3000",
+		cache: new InMemoryCache(),
+	});
+	setClient(client);
+
+	const q = gql`
+		query Users {
+			users {
+				id
+				name
+			}
+		}
+	`;
+
+	const res = query(q);
+	console.log("res:", res);
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
+
+	{#if $res.loading}
+		Loading...
+	{:else if $res.error}
+		Error: {$res.error}
+	{:else}
+		{JSON.stringify($res.data)}
+	{/if}
 </main>
 
 <style>
