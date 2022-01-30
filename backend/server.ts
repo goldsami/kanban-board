61a1a5a913139c7487768dc53bcaf2a8f38ea252
goldsami <zolotarenkomax@gmail.com>
2022-01-30 21:08:23 +0200

    feat: cors issue fixed

--- backend/server.ts
+++ backend/server.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { Schema } from './presentation';
-
+import cors from 'cors';
 
 async function start() {
   try {
-
-    const port = 3000;
+    const port = process.env.PORT || 3000;
     const app = express();
+    app.use(cors({
+      origin: process.env.BACKEND_URL || ''
+    }));
     app.use('/', graphqlHTTP({
       schema: Schema,
       graphiql: true,
