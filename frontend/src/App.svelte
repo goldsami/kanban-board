ca8d418f02af788d7497210e471bb55621874e7d
goldsami <zolotarenkomax@gmail.com>
2022-02-03 21:24:56 +0200

    feat(frontend): firebase removed, create user query added

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,21 +2,6 @@
 	import Q from "./components/Q.svelte";
 	import Auth from "./components/Auth.svelte";
 	export let name;
-
-	import { initializeApp } from "firebase/app";
-
-	const firebaseConfig = {
-		apiKey: __myapp["env"].FIREBASE_API_KEY,
-		authDomain: __myapp["env"].FIREBASE_AUTH_DOMAIN,
-		projectId: __myapp["env"].FIREBASE_PROJECT_ID,
-		storageBucket: __myapp["env"].FIREBASE_STORAGE_BUCKET,
-		messagingSenderId: __myapp["env"].FIREBASE_MESSAGE_SENDING_ID,
-		appId: __myapp["env"].FIREBASE_APP_ID,
-		measurementId: __myapp["env"].FIREBASE_MEASUREMENT_ID,
-	};
-	console.log("config", firebaseConfig);
-
-	const app = initializeApp(firebaseConfig);
 </script>
 
 <main>
--- frontend/src/components/Auth.svelte
+++ frontend/src/components/Auth.svelte
@@ -1,9 +1,18 @@
 <script>
   import axios from "axios";
 
-  function createUser(email = "email@gmail.com", password = "password") {}
+  function createUser(email = "email11@gmail1.com", password = "password") {
+    console.log("create user");
+    axios
+      .post("http://localhost:3000/sign-up", {
+        email,
+        password,
+      })
+      .then((x) => console.log("sing-up:", x));
+  }
 
   function login(email = "email@gmail.com", password = "password") {
+    console.log("login");
     axios
       .post("http://localhost:3000/login", {
         email,
