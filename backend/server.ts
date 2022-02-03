465292d7599b59d1dfb236570c3231a0db268778
goldsami <zolotarenkomax@gmail.com>
2022-02-03 20:55:30 +0200

    feat(backend): login route added

--- backend/server.ts
+++ backend/server.ts
@@ -4,10 +4,35 @@ import { Schema } from './presentation';
 import cors from 'cors';
 // import { defalult as serviceAccount } from 'firebase/kanban-board-175b9-firebase-adminsdk-m9m5x-ef3afb6c6a.json';
 import config from './firebase/config.json';
+// import admin from 'firebase-admin';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const admin = require('firebase-admin');
+
+const authMiddleware = (req, res, next) => {
+
+  if (req.headers.authorization) {
+    const tokenParts = req.headers.authorization
+      .split(' ')[1];
+    // console.log('token:', tokenParts);
+    admin.auth().verifyIdToken(tokenParts).then(x => {
+      res.locals.userId = x.user_id;
+      next();
+    }).catch((e) => {
+      console.log('err', e.message);
+      next();
+      // throw '401';
+    });
+  }
+  else throw '402';
+};
 
 async function start() {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const admin = require('firebase-admin');
 
   admin.initializeApp({
     credential: admin.credential.cert(config),
@@ -18,22 +43,27 @@ async function start() {
     app.use(cors({
       origin: process.env.BACKEND_URL || ''
     }));
-    app.use((req, res, next) => {
+    app.post('/login', express.json(), async (req, res) => {
+      const body = req.body;
+      console.log('body:', body);
+      const auth = getAuth();
+      signInWithEmailAndPassword(auth, req.body.email, req.body.password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          user.getIdToken().then(token => {
+            res.send(token);
+          });
+          // ...
+        })
+        .catch((error) => {
+          console.log('err', error.message);
 
-      if (req.headers.authorization) {
-        const tokenParts = req.headers.authorization
-          .split(' ')[1];
-        // console.log('token:', tokenParts);
-        admin.auth().verifyIdToken(tokenParts).then(x => {
-          res.locals.userId = x.user_id;
-          next();
-        }).catch(() => {
-          throw '401';
+          const errorCode = error.code;
+          const errorMessage = error.message;
         });
-      }
-      else throw '401';
     });
-    app.use('/graphql', (req, res) => {
+    app.use('/graphql', authMiddleware, (req, res) => {
       graphqlHTTP({
         schema: Schema,
         graphiql: true,
