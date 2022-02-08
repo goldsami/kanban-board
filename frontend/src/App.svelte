449dc8fdfc7df8b7118d6cf184366bf9e5bea5a1
goldsami <zolotarenkomax@gmail.com>
2022-02-08 21:11:31 +0200

    feat(frontend): routing package added

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,13 +1,19 @@
 <script>
-	import Q from './components/Q.svelte';
-	import Auth from './components/Auth.svelte';
-	export let name;
+	import { Router, Route, Link } from "svelte-navigator";
 </script>
 
-<main>
-	<Q {name} />
-	<Auth />
-</main>
+<Router>
+	<nav>
+		<Link to="/">Home</Link>
+		<Link to="about">About</Link>
+		<Link to="blog">Blog</Link>
+	</nav>
+	<div>
+		<Route path="/">home</Route>
+		<Route path="about">about</Route>
+		<Route path="blog/*">blog</Route>
+	</div>
+</Router>
 
 <style>
 </style>
