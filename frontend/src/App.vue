c617fed0a8ffdba141dd45bd511dd242daad2b86
goldsami <zolotarenkomax@gmail.com>
2022-02-13 22:34:33 +0200

    style(frontend): navbar added from materialize

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div style="margin-left: 4px">
+    <nav id="nav" class="teal">
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo">Logo</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/about">About</router-link></li>
+          <li><router-link to="/login">Login</router-link></li>
+        </ul>
+      </div>
+    </nav>
     <router-view />
   </div>
 </template>
