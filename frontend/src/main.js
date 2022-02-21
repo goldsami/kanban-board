d58d098f84a455e27ac96ba2c83b2abb4d9cf75f
goldsami <zolotarenkomax@gmail.com>
2022-02-21 21:41:06 +0200

    refactor(frontend): comments removed

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,10 +11,7 @@ import { store } from './store';
 import 'materialize-css/dist/css/materialize.min.css';
 
 const authMiddleware = new ApolloLink((operation, forward) => {
-  // add the authorization to the headers
-  // console.log('middle', store);
   const { token } = store;
-  // console.log('bear');
   operation.setContext({
     headers: {
       authorization: token ? `Bearer ${token}` : null,
@@ -24,16 +21,12 @@ const authMiddleware = new ApolloLink((operation, forward) => {
   return forward(operation);
 });
 
-// HTTP connection to the API
 const httpLink = createHttpLink({
-  // You should use an absolute URL here
   uri: 'https://obscure-forest-51635.herokuapp.com/graphql',
 });
 
-// Cache implementation
 const cache = new InMemoryCache();
 
-// Create the apollo client
 const apolloClient = new ApolloClient({
   link: authMiddleware.concat(httpLink),
   cache,
@@ -53,19 +46,12 @@ app.directive('cl-out', {
   },
   mounted(el, binding, vnode) {
     el.clickOutsideEvent = (event) => {
-      // here I check that click was outside the el and his children
-      console.log('click outside', { vnode, binding, event });
       binding.value();
     };
     document.body.addEventListener('click', el.clickOutsideEvent);
   },
   unmounted(el) {
     document.body.removeEventListener('click', el.clickOutsideEvent);
-  },
-  // When the bound element is inserted into the DOM...
-  inserted(el) {
-    // Focus the element
-    console.log('instedted', el);
   },
 });
 
